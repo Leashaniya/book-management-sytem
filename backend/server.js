473a37b6f43096fed82c9f105ec9fa685592817c
Leashaniya <leashakrish2002@gmail.com>
2024-05-20 12:04:39 +0530

    node modules

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const cors=require("cors");
+const express=require("express");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const app= express();  
+
+const PORT =process.env.PORT || 8000;
+app.listen(PORT,() =>{
+    console.log("server is up and running on port")
+});
+
+const URL=process.env.MONGODB_URL;
+mongoose.connect(URL,);
+
+const connection =mongoose.connection;
+connection.once("open",()=>{
+    console.log("Mongodb connection success")
+});
+
+app.use(cors());
+app.use(bodyParser.json());
